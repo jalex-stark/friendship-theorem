4bbf34066883d602241f9810c8f0cb6b534bdd9f
jalex-stark <alexmaplegm@gmail.com>
2020-06-19 19:38:21 -0400

    yury optimized a proof in chat

--- src/tensor.lean
+++ src/tensor.lean
@@ -50,13 +50,9 @@ end
 -- f : A ⊗[R] B ≃ₗ[R] C)
 example : matrix n n A →ₗ[R] A ⊗[R] matrix n n R :=
 { to_fun := λ x, ∑ i j : n, (x i j) ⊗ₜ[R] (elem_matrix i j),
-    map_add' := by { intros,
-      rw ← sum_add_distrib,
-      conv_rhs { congr, skip, funext, rw ← sum_add_distrib,}, 
-      apply sum_congr, {refl}, intro i, norm_num,
-      apply sum_congr, {refl}, intro j, norm_num,
-      rw add_tmul },
-    map_smul' := by { intros, simp_rw smul_sum, congr }}
+    map_add' := by { intros, simp only [add_tmul, sum_add_distrib, matrix.add_val] },
+    map_smul' := by { intros, simp_rw smul_sum, congr }
+}
 
 
 def matrix_lin_equiv : matrix n n A ≃ₗ[R] A ⊗[R] matrix n n R :=
@@ -68,5 +64,5 @@ example : matrix n n A ≃ₐ[R] A ⊗[R] matrix n n R :=
 begin
   symmetry, 
   -- why doesn't this work?
-  have := alg_equiv_of_linear_equiv_tensor_product matrix_lin_equiv,
+  simp_rw alg_equiv_of_linear_equiv_tensor_product,
 end
